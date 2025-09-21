f117811798db3be9f8250a1252021d892ac33680
ShihoTL <mgbekenjustice2@gmail.com>
2025-09-21 08:58:39 +0100

    bruh

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 //@ts-ignore
-import "@fontsource/jost"; // Defaults to weight 400
-import "@fontsource/jost/400.css"; // Specify weight
-import "@fontsource/jost/400-italic.css";
+import "@fontsource/caveat-brush";
+
+// import "@fontsource/jost"; // Defaults to weight 400
+// import "@fontsource/jost/400.css"; // Specify weight
+// import "@fontsource/jost/400-italic.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
